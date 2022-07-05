2364e33b3d25e6acdc52782c1fe682ae946b5f25
bilalcinal <73084109+bilalcinal@users.noreply.github.com>
2022-07-05 15:11:28 +0300

    Renk butonu ve panel
    renk butonu, Panel eklendi renk butonuna mouse u götürüp tıkladığımızda farklı üzerine götürdüğümüzde farklı renk çıkıyor

--- Paint-App/Paint-App/Form1.Designer.cs
+++ Paint-App/Paint-App/Form1.Designer.cs
@@ -28,12 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Pic_renk = new System.Windows.Forms.Button();
+            this.Pic = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Info;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.Pic_renk);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(100, 503);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Image = global::Paint_App.Properties.Resources.color;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(3, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(69, 66);
+            this.button1.TabIndex = 1;
+            this.button1.Text = " Renk";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Pic_renk
+            // 
+            this.Pic_renk.BackColor = System.Drawing.Color.White;
+            this.Pic_renk.Location = new System.Drawing.Point(3, 81);
+            this.Pic_renk.Name = "Pic_renk";
+            this.Pic_renk.Size = new System.Drawing.Size(57, 33);
+            this.Pic_renk.TabIndex = 0;
+            this.Pic_renk.UseVisualStyleBackColor = false;
+            // 
+            // Pic
+            // 
+            this.Pic.BackColor = System.Drawing.Color.White;
+            this.Pic.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Pic.Location = new System.Drawing.Point(0, 0);
+            this.Pic.Name = "Pic";
+            this.Pic.Size = new System.Drawing.Size(782, 503);
+            this.Pic.TabIndex = 1;
+            this.Pic.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(782, 503);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Pic);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Pic)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Panel panel1;
+        private PictureBox Pic;
+        private Button button1;
+        private Button Pic_renk;
     }
 }
