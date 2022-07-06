55c3dac7de0788f646d97e6c9813a18a93bb80de
bilalcinal <73084109+bilalcinal@users.noreply.github.com>
2022-07-06 18:56:22 +0300

    Final sürüm
    Bütün butonlar stabil bir şekilde çalışıyor.

--- Paint-App/Paint-App/Form1.cs
+++ Paint-App/Paint-App/Form1.cs
@@ -14,14 +14,14 @@ namespace Paint_App
             g = Graphics.FromImage(bm);
             g.Clear(Color.White);
             pic.Image = bm;
-            
+
         }
-        
+
         Bitmap bm;
         Graphics g;
         bool paint = false;
         Point px, py;
-        Pen p =new Pen(Color.Black,1);
+        Pen p = new Pen(Color.Black, 1);
         Pen erase = new Pen(Color.White, 40);
         int index;
         int x, y, sX, sY, cX, cY;
@@ -29,13 +29,13 @@ namespace Paint_App
         ColorDialog cd = new ColorDialog();
         Color new_color;
 
-       
+
 
         private void pic_MouseMove(object sender, MouseEventArgs e)
         {
             if (paint)
             {
-                if (index==1)
+                if (index == 1)
                 {
                     px = e.Location;
                     g.DrawLine(p, px, py);
@@ -58,7 +58,7 @@ namespace Paint_App
 
         }
 
-       
+
 
         private void pic_MouseDown(object sender, MouseEventArgs e)
         {
@@ -69,7 +69,7 @@ namespace Paint_App
 
         }
 
-      
+
 
         private void pic_MouseUp(object sender, MouseEventArgs e)
         {
@@ -77,23 +77,27 @@ namespace Paint_App
             sX = x - cX;
             sY = y - cY;
 
-            if (index==3)
+            if (index == 3)
             {
                 g.DrawEllipse(p, cX, cY, sX, sY);
             }
-            if (index==4)
+            if (index == 4)
             {
                 g.DrawRectangle(p, cX, cY, sX, sY);
             }
-            if (index==5)
+            if (index == 5)
             {
-                g.DrawLine(p,cX,cY,x,y);
+                g.DrawLine(p, cX, cY, x, y);
             }
-         
+
         }
 
         private void btn_temizle_Click(object sender, EventArgs e)
         {
+            this.Width = 900;
+            this.Height = 700;
+            bm = new Bitmap(pic.Width, pic.Height);
+            g = Graphics.FromImage(bm);
             g.Clear(Color.White);
             pic.Image = bm;
             index = 0;
@@ -112,13 +116,13 @@ namespace Paint_App
             index = 1;
         }
 
-       
+
 
         private void btn_silgi_Click(object sender, EventArgs e)
         {
             index = 2;
         }
-        
+
         private void btn_elips_Click(object sender, EventArgs e)
         {
             index = 3;
@@ -129,7 +133,7 @@ namespace Paint_App
             index = 4;
         }
 
-       
+
 
         private void btn_çizgi_Click(object sender, EventArgs e)
         {
@@ -138,19 +142,43 @@ namespace Paint_App
 
         private void btn_yükle_Click(object sender, EventArgs e)
         {
-            var bitmap = new Bitmap(@"C:\Users\hbila\OneDrive\Masaüstü\txt.bmp");
-            g = Graphics.FromImage(bitmap);
-            pic.Image = bitmap;
+
+           
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var bitmap = new Bitmap(openFileDialog1.FileName);
+                    g = Graphics.FromImage(bitmap);
+                    pic.Image = bitmap;
+
+                }
+                catch
+                {
+                    MessageBox.Show("ERROR! Please check passphrase and do not attempt to edit cipher text");
+                }
+            }
 
 
         }
 
         private void btn_kaydet_Click(object sender, EventArgs e)
         {
-            string fileName = @"C:\Users\hbila\OneDrive\Masaüstü\txt.bmp";
+           
             Bitmap bmp = new Bitmap(pic.ClientSize.Width, pic.ClientSize.Width);
             pic.DrawToBitmap(bmp, pic.ClientRectangle);
-            bmp.Save(fileName, ImageFormat.Bmp);
+            SaveFileDialog sf = new SaveFileDialog();
+            sf.Filter = "Bitmap Image (.bmp)|.bmp|Gif Image (.gif)|.gif|JPEG Image (.jpeg)|.jpeg|Png Image (.png)|.png|Tiff Image (.tiff)|.tiff|Wmf Image (.wmf)|.wmf";
+            sf.ShowDialog();
+            var path = sf.FileName;
+            bmp.Save(path, ImageFormat.Bmp);
         }
 
         private void pic_Paint(object sender, PaintEventArgs e)
@@ -174,17 +202,17 @@ namespace Paint_App
             }
         }
 
-        
+
         static Point set_point(PictureBox pb, Point pt)
         {
             float pX = 1f * pb.Image.Width / pb.Width;
             float pY = 1f * pb.Image.Height / pb.Height;
-            return new Point((int)(pt.X*pX),(int)(pt.Y*pY));
+            return new Point((int)(pt.X * pX), (int)(pt.Y * pY));
         }
 
-       
 
-        public void Doldur(Bitmap bm , int x, int y, Color new_rnk)
+
+        public void Doldur(Bitmap bm, int x, int y, Color new_rnk)
         {
             Color old_color = bm.GetPixel(x, y);
             Stack<Point> pixel = new Stack<Point>();
@@ -192,24 +220,24 @@ namespace Paint_App
             bm.SetPixel(x, y, new_rnk);
             if (old_color == new_rnk) return;
 
-            while(pixel.Count > 0)
+            while (pixel.Count > 0)
             {
                 Point pt = (Point)pixel.Pop();
-                if (pt.X>0 && pt.Y>0 && pt.X<bm.Width-1 && pt.Y<bm.Height-1)
+                if (pt.X > 0 && pt.Y > 0 && pt.X < bm.Width - 1 && pt.Y < bm.Height - 1)
                 {
                     validate(bm, pixel, pt.X - 1, pt.Y, old_color, new_rnk);
-                    validate(bm, pixel, pt.X , pt.Y - 1, old_color, new_rnk);
+                    validate(bm, pixel, pt.X, pt.Y - 1, old_color, new_rnk);
                     validate(bm, pixel, pt.X + 1, pt.Y, old_color, new_rnk);
-                    validate(bm, pixel, pt.X , pt.Y + 1, old_color, new_rnk);
+                    validate(bm, pixel, pt.X, pt.Y + 1, old_color, new_rnk);
                 }
             }
         }
-        private void validate(Bitmap bm,Stack<Point>sp,int x,int y,Color old_color,Color new_color)
+        private void validate(Bitmap bm, Stack<Point> sp, int x, int y, Color old_color, Color new_color)
         {
             Color cx = bm.GetPixel(x, y);
-            if (cx==old_color)
+            if (cx == old_color)
             {
-                sp.Push(new Point(x,y));
+                sp.Push(new Point(x, y));
                 bm.SetPixel(x, y, new_color);
 
             }
@@ -217,7 +245,7 @@ namespace Paint_App
 
         private void pic_MouseClick(object sender, MouseEventArgs e)
         {
-            if (index==6)
+            if (index == 6)
             {
                 Point point = set_point(pic, e.Location);
                 Doldur(bm, point.X, point.Y, new_color);
@@ -229,7 +257,7 @@ namespace Paint_App
             index = 6;
         }
 
-       
+
 
 
     }
